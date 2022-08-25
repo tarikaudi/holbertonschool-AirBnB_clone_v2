3084824a401089b3acc3bf532f6ca2121e133a76
root <tarik.audi@hotmail.com>
2022-08-25 18:37:01 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 # comment for the checker
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "
-<html>
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current 
-chown -R ubuntu:ubuntu /data/
-sed -i "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-enabled/default
+</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '$ d' /etc/nginx/sites-available/default
+echo "location /hbnb_static {
+alias /data/web_static/current/;
+error_page 404 /custom_404.html;
+try_files \$uri \$uri/ =404;
+}
+}" >> /etc/nginx/sites-available/default
 sudo service nginx restart
