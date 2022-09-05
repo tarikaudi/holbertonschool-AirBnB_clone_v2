7aecf4ae4cd629ae298397e447726dbda05a0f85
root <tarik.audi@hotmail.com>
2022-09-05 18:13:07 +0000

    pep

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -16,13 +16,15 @@ def home():
     """def home fun"""
     return ("HBNB")
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """parse text __"""
     parse_text = text.replace('_', ' ')
     return "{}".format(parse_text)
 
-@app.route('/python')
+
+@app.route('/python', defaults={'text': "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_etc(text="is cool"):
     """python etc fun"""
