4509c45f1fe191ee12a67ea966e610ea5cecb414
root <tarik.audi@hotmail.com>
2022-09-05 19:17:10 +0000

    state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from datetime import datetime
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    type_storage = os.getenv("HBNB_TYPE_STORAGE")
+    __tablename__ = "states"
+
+    if type_storage == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        name = ""
+
+    @property
+    def cities(self):
+        """devolver una lista de instancias de city"""
+        inst_list = []
+        list_objects = models.storage.all(City)
+        for city in list_objects.values():
+            if city.state_id == self.id:
+                inst_list.append(city)
+        return inst_list
