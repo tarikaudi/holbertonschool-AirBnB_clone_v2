106c73009832cd470f5c7c82fe03e220510437c8
root <tarik.audi@hotmail.com>
2022-09-05 20:18:26 +0000

    bronken

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+import os
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 
-storage = FileStorage()
+db_value = os.getenv("HBNB_TYPE_STORAGE")
+if db_value == "db":
+    storage = DBStorage()
+else:
+
+    storage = FileStorage()
 storage.reload()
