b2b2186971af9fbf6e1a971fe18f134b728e904c
root <tarik.audi@hotmail.com>
2022-09-05 18:27:42 +0000

    test

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def is_int(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def link(n):
     """link with template"""
-    return (render_template('5-number.html', n=n))
+    return (render_template('5-number.html', number=n))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
